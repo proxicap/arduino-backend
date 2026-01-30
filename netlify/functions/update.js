bddafc024a92939b6383c421a333d1aa324fbe1b
proxicap <proxicapmail@gmail.com>
2026-01-30 01:29:53 -0700

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,18 +2,33 @@ let latestData = {};
 
 exports.handler = async (event) => {
   if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
+    return {
+      statusCode: 405,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
+      body: "Method Not Allowed"
+    };
   }
 
   try {
     latestData = JSON.parse(event.body);
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
       body: JSON.stringify({ status: "ok" })
     };
   } catch (err) {
     return {
       statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
       body: "Invalid JSON"
     };
   }
